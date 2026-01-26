17bdfa1cc6b5fd9760a1e2227f6bd923d62e7b05
Gagajew <wi23b164@technikum-wien.at>
2026-01-26 15:21:16 +0100

    permissions for URL set

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    //public
     {
       path: '/',
       name: 'home',
       component: HomeView,
-    },
-    {
-      path: '/posts',
-      name: 'posts',
-      component: () => import('../views/PostView.vue'),
-    },
-    {
-      path: '/posts/:title',
-      name: 'post-detail',
-      component: () => import('../views/PostView.vue'),
-    },
-    {
-      path: '/users',
-      name: 'users',
-      component: () => import('../views/UserView.vue'),
-    },
-    {
-      path: '/users/:name',
-      name: 'users-detail',
-      component: () => import('../views/UserView.vue'),
-    },
-    {
-      path: '/groups',
-      name: 'groups',
-      component: () => import('../views/GroupView.vue'),
-    },
-    {
-      path: '/groups/:name',
-      name: 'groups-detail',
-      component: () => import('../views/GroupView.vue'),
     },
     {
       path: '/login',
@@ -59,17 +31,123 @@ const router = createRouter({
       name: 'imprint',
       component: ()  => import('../views/ImprintView.vue'),
     },
+
+    //users
+    {
+      path: '/posts',
+      name: 'posts',
+      component: () => import('../views/PostView.vue'),
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/posts/:title',
+      name: 'post-detail',
+      component: () => import('../views/PostView.vue'),
+      meta: {
+        requiresAuth: true
+      }
+    },
+    
+    
+    {
+      path: '/groups',
+      name: 'groups',
+      component: () => import('../views/GroupView.vue'),
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/groups/:name',
+      name: 'groups-detail',
+      component: () => import('../views/GroupView.vue'),
+      meta: {
+        requiresAuth: true
+      }
+    },
+
+
+    //only admin
+    {
+      path: '/users',
+      name: 'users',
+      component: () => import('../views/UserView.vue'),
+      meta: {
+        requiresAuth: true,
+        role: "ADMIN"
+      }
+    },
+    {
+      path: '/users/:name',
+      name: 'users-detail',
+      component: () => import('../views/UserView.vue'),
+      meta: {
+        requiresAuth: true,
+        role: "ADMIN"
+      }
+    },
+
+    {
+      path: '/users/:id',
+      name: 'user-admin-edit',
+      component: () => import("../views/ProfileView.vue"),
+      meta:{
+        requiresAuth: true,
+        role: "ADMIN"
+      }
+    },
     {
       path: '/profile',
       name: 'profile',
       component: () => import('../views/ProfileView.vue'),
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/profile/:section',
       name: 'profile-section',
       component: () => import('../views/ProfileView.vue'),
+      meta: {
+        requiresAuth: true
+      }
     },
   ],
 })
+
+import { useAuthStore } from '@/stores/auth';
+
+router.beforeEach((to) => {
+  const auth = useAuthStore();
+
+  const requiresAuth = to.matched.some(r => r.meta?.requiresAuth);
+  const requiredRole = to.meta?.role;
+
+  if(requiresAuth && !auth.isLoggedIn){
+    return{
+      path: "/login",
+      query: {
+        redirect: to.fullPath
+      },
+    };
+  }
+
+
+    if(requiredRole && auth.role !== requiredRole){
+      return{
+        path: "/"
+      };
+    }
+
+    if(auth.isLoggedIn && (to.path === "/login" || to.path === "/register")){
+      return {
+        path: "/"
+      };
+    }
+
+    return true; 
+});
 
 export default router
